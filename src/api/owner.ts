ecc73777ffaf2ad349b6b354e9f67473bb015617
Tomáš Kretek <kretek@inqool.cz>
2024-10-07 19:31:21 +0200

    Added new owners sigla check

--- src/api/owner.ts
+++ src/api/owner.ts
@@ -2,6 +2,7 @@ import { useMutation, useQuery } from '@tanstack/react-query'
 import clone from 'lodash/clone'
 import { api, queryClient } from './index'
 import { TEditableOwner, TOwner } from '../schema/owner'
+import ky from 'ky'
 
 export const useOwnerListQuery = () => {
   return useQuery({
@@ -31,7 +32,6 @@ export const useCreateOwnerMutation = () =>
     mutationFn: (owner: TEditableOwner) => {
       const ownerClone = clone(owner)
       ownerClone.name = ownerClone.name.trim()
-      ownerClone.sigla = ownerClone.sigla.trim()
 
       return api().post(`owner`, { json: ownerClone }).json<void>()
     },
@@ -39,3 +39,23 @@ export const useCreateOwnerMutation = () =>
       queryClient.invalidateQueries({ queryKey: ['owner', 'list'] })
     },
   })
+
+type TSiglaList = {
+  resultCount: number
+  records?: { sigla: string; id: string; title: string; urls: string[] }[]
+  status: 'OK'
+}
+
+export type TSiglaListError = {
+  status: 'ERROR'
+  statusMessage: string
+}
+
+export const useGetSiglaListMutation = () => {
+  return useMutation<TSiglaList, TSiglaListError, string>({
+    mutationFn: (sigla) =>
+      ky(
+        `https://www.knihovny.cz/api/v1/libraries/search?lookfor=${sigla}&type=Sigla&sort=relevance&page=1&limit=20&prettyPrint=false&lng=cs`
+      ).json(),
+  })
+}
