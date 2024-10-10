92e771be647b75dc54f380fb3f909d5fce10586c
Tomáš Kretek <kretek@inqool.cz>
2024-10-10 15:10:21 +0200

    Fixed build for a public and editing section

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,12 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  readonly NODE_ENV: string
+  readonly SENTRY_AUTH_TOKEN: string
+  readonly SENTRY_ORG: string
+  readonly SENTRY_PROJECT: string
+  readonly SENTRY_URL: string
   readonly VITE_SENTRY_DNS: string
-  readonly VITE_SENTRY_AUTH_TOKEN: string
-  readonly VITE_SENTRY_ORG: string
-  readonly VITE_SENTRY_PROJECT: string
-  readonly VITE_SENTRY_URL: string
   readonly VITE_MUI_LICENCE_KEY: string
   readonly VITE_APP_TYPE: string
 }
