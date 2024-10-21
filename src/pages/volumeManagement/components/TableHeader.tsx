801dc13ea70e090680071bd48bc45f36f72c4f5a
Tomáš Kretek <kretek@inqool.cz>
2024-10-21 14:13:06 +0200

    Added rows count into volume management

--- src/pages/volumeManagement/components/TableHeader.tsx
+++ src/pages/volumeManagement/components/TableHeader.tsx
@@ -34,18 +34,29 @@ const TableHeader = () => {
       <Box
         sx={{
           display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'flex-start',
-          gap: '2px',
-          paddingRight: '8px',
+          gap: '20px',
+          alignItems: 'flex-end',
         }}
       >
         <Typography>
-          {`${t('volume_overview.volume_created_at')}: ${dayjs(data?.volume?.created).isValid() ? dayjs(data?.volume?.created).format('DD.MM.YYYY HH:mm:ss') : '---'}`}
+          {`${t('volume_overview.rows_count')}: ${data?.specimens.length}`}
         </Typography>
-        <Typography>
-          {`${t('volume_overview.volume_updated_at')}: ${dayjs(data?.volume?.updated).isValid() ? dayjs(data?.volume?.updated).format('DD.MM.YYYY HH:mm:ss') : '---'}`}
-        </Typography>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'flex-start',
+            gap: '2px',
+            paddingRight: '8px',
+          }}
+        >
+          <Typography>
+            {`${t('volume_overview.volume_created_at')}: ${dayjs(data?.volume?.created).isValid() ? dayjs(data?.volume?.created).format('DD.MM.YYYY HH:mm:ss') : '---'}`}
+          </Typography>
+          <Typography>
+            {`${t('volume_overview.volume_updated_at')}: ${dayjs(data?.volume?.updated).isValid() ? dayjs(data?.volume?.updated).format('DD.MM.YYYY HH:mm:ss') : '---'}`}
+          </Typography>
+        </Box>
       </Box>
     </Box>
   )
